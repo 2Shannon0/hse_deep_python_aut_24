1360912002be0858d407c17ae5069fcd32880ad7
2Shannon0 <93452905+2Shannon0@users.noreply.github.com>
2024-11-26 20:08:58 +0300

    HW5 ПРОБА 2

--- 05/test_lru_cache.py
+++ 05/test_lru_cache.py
@@ -49,3 +49,33 @@ class TestLRUCache(unittest.TestCase):
 
         self.assertEqual(cache.get("k1"), "new_val1")
         self.assertEqual(cache.get("k2"), "val2")
+
+    def test5_limit_negative(self):
+        with self.assertRaises(ValueError):
+            LRUCache(-100)
+
+    def test6_set_with_lru_1(self):
+        cache = LRUCache(1)
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+
+        self.assertEqual(cache["k1"], None)
+        self.assertEqual(cache["k2"], "val2")
+
+    def test7_change_value_(self):
+        cache = LRUCache(3)
+
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+        cache.set("k3", "val3")
+
+        self.assertEqual(cache["k1"], "val1")
+        cache.set("k1", "777")
+        self.assertEqual(cache["k1"], "777")
+
+        cache.set("k4", "val4")
+
+        self.assertEqual(cache["k1"], "777")
+        self.assertEqual(cache["k2"], None) # вытес не измененный ранее элемент
+        self.assertEqual(cache["k3"], "val3")
+        self.assertEqual(cache["k4"], "val4")
